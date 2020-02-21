c26a15481db39daaecd3a00d84d1dc2828d4a194
rahulgidde <rahulgidde1212@gmail.com>
2020-02-21 10:32:08 +0530

    ADDED:Gambler make  bet so check either win or loss

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -4,4 +4,15 @@
 STAKE=100
 BET=1
 
+#GENERATE RANDOM NUMBER FOR GAMBLER
+randomcheck=$((RANDOM%2))
 
+#CHECK IF WIN OR LOOSE BY $1
+cash=$STAKE
+if [ $randomcheck -eq $BET ]
+then
+	((cash++))
+else
+	((cash--))
+fi
+echo "Cash: " $cash
