c3fe1d741a3f922a235bd278e42d16ffc21782b0
rahulgidde <rahulgidde1212@gmail.com>
2020-02-21 11:01:54 +0530

    ADDED:Gambler won or loss 50% of Stake then resign for the day

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -3,16 +3,28 @@
 #CONSTANT
 STAKE=100
 BET=1
+MAXWIN=$(($STAKE+$STAKE/2))
+MINLOSS=$(($STAKE/2))
 
-#GENERATE RANDOM NUMBER FOR GAMBLER
-randomcheck=$((RANDOM%2))
+#VARIABLE
+cash=0
 
-#CHECK IF WIN OR LOOSE BY $1
-cash=$STAKE
-if [ $randomcheck -eq $BET ]
-then
-	((cash++))
-else
-	((cash--))
-fi
-echo "Cash: " $cash
+#FUNCTION TO GET DAILY CASH WIN OR LOOSE
+function gambling
+{
+	cash=$STAKE
+	while [[ $cash -ne $MAXWIN  && $cash -ne $MINLOSS ]]
+	do
+		randomcheck=$((RANDOM%2))
+		if [ $randomcheck -eq $BET ]
+		then
+			((cash++))
+		else
+			((cash--))
+		fi
+	done
+	echo "Cash: " $cash
+}
+
+#FUNCTION CALL
+gambling
